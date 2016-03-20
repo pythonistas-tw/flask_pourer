0a6b970b952b95cb3be1847d1c95e6673987bf93
Theon Lin <theon_lin@petple.co>
2016-03-20 12:49:27 +0800

    Add Setup.py.

---
+++ flask_pourer/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Pythonistas Taiwan'
+__email__ = 'contact@pythonistas.tw'
---
+++ setup.py
@@ -0,0 +1,33 @@
+'''
+Flask-Pourer
+------------
+Flask-Pourer is an extension for Flask that adds support for JSONAPI with Mongodb to your application.
+'''
+from setuptools import setup
+
+setup(
+    name='Flask-Pourer',
+    version='0.0.1',
+    url='https://github.com/pythonistas-tw/flask_pourer',
+    license='MIT',
+    author='Pythonistas Taiwan',
+    author_email='contact@pythonistas.tw',
+    description='Extension of JSONAPI for flask',
+    long_description=__doc__,
+    py_modules=['flask_pourer'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'pymongo'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
